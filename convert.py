73d5bcf24298e72772f0d7e158ed38a8de2f95ac
wutianyu666 <grade6_wty@163.com>
2022-08-16 18:40:25 +0800

    Now it gains the ability of transforming all the pictures at one time.

--- convert.py
+++ convert.py
@@ -8,19 +8,24 @@ OUTPUT = os.path.join(WORKSPACE, "result/")
 print(WORKSPACE)
 
 def main():
-    block = int(input("block:"))
-    file = input("path:")
     pygame.init()
-    img = pygame.image.load(os.path.join(WORKSPACE, file + ".jpg"))
-    width, height = img.get_size()
-    result = pygame.Surface((width, height))
-    img.lock()
-    result.lock()
-    for i in range(block):
-        for x in range(width):
-            for y in range(height // block):
-                result.set_at((x, (height // block * i) + y), img.get_at((x, height // block * (block - 1 - i) + y)))
-    pygame.image.save(result, os.path.join(OUTPUT, file + ".bmp"))
+    for root, dirs, files in os.walk(WORKSPACE):
+        if root != WORKSPACE:
+            continue
+        for file in files:
+            if file[-4:] != ".jpg":
+                continue
+            block = int(input(file + " block:"))
+            img = pygame.image.load(os.path.join(WORKSPACE, file))
+            width, height = img.get_size()
+            result = pygame.Surface((width, height))
+            img.lock()
+            result.lock()
+            for i in range(block):
+                for x in range(width):
+                    for y in range(height // block):
+                        result.set_at((x, (height // block * i) + y), img.get_at((x, height // block * (block - 1 - i) + y)))
+            pygame.image.save(result, os.path.join(OUTPUT, file[0:-4] + ".bmp"))
 
 if __name__ == "__main__":
     main()
